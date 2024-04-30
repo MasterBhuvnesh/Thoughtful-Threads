3a9fc549c0a36b7d7f8d88538163236da8fccaba
Bhuvnesh verma <99537126+MasterBhuvnesh@users.noreply.github.com>
2024-04-30 18:34:58 +0530

    New line in back server

--- src/testimonials/data.jsx
+++ src/testimonials/data.jsx
@@ -1,8 +1,27 @@
-import Written from './quotes.json';
- function selectRandomItems(arr, numItems) {
-    const shuffled = arr.sort(() => 0.5 - Math.random());
-    return shuffled.slice(0, numItems);
-  }
-export  const Data = selectRandomItems(Written, 3);
+// import data from "./quotes.json";
+// function selectRandomItems(arr, numItems) {
+//   const shuffled = arr.sort(() => 0.5 - Math.random());
+//   return shuffled.slice(0, numItems);
+// }
+// export const Data = selectRandomItems(data, 3);
+import axios from "axios";
 
-    
+function selectRandomItems(arr, numItems) {
+  const shuffled = arr.sort(() => 0.5 - Math.random());
+  return shuffled.slice(0, numItems);
+}
+
+export const fetchData = async () => {
+  try {
+    const response = await axios.get("https://qoute-back-server.vercel.app/");
+    return response.data;
+  } catch (error) {
+    console.error("Failed to fetch data:", error);
+    return [];
+  }
+};
+
+export const Data = async () => {
+  const data = await fetchData();
+  return selectRandomItems(data, 3);
+};
--- src/testimonials/testimonials.jsx
+++ src/testimonials/testimonials.jsx
@@ -1,10 +1,23 @@
+import React, { useEffect, useState } from "react";
 import "./testimonials.css";
 import { Data } from "./data";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import "swiper/css/pagination";
 import { Pagination } from "swiper/modules";
+
 const Testimonials = () => {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const fetchedData = await Data();
+      setData(fetchedData);
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <section className="testimonial container section">
       <h2 className="section_title">Unlocking Wisdom</h2>
@@ -29,14 +42,15 @@ const Testimonials = () => {
         }}
         modules={[Pagination]}
       >
-        {Data.map(({ id,author ,quote}) => {
-          return (
-            <SwiperSlide className="testimonial_card" key={id}>
-              <h3 className="testimonial_author">{author}</h3>
-              <p className="testimonial_quote" >{quote}</p>
-            </SwiperSlide>
-          );
-        })}
+        {data.map(({ id, author, quote }) => (
+          <SwiperSlide
+            className="testimonial_card"
+            key={id}
+          >
+            <h3 className="testimonial_author">{author}</h3>
+            <p className="testimonial_quote">{quote}</p>
+          </SwiperSlide>
+        ))}
       </Swiper>
     </section>
   );
